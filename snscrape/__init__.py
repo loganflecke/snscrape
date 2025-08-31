6794d7bea9d159e65f9ff2c751b16425624880d0
Logan <97614921+loganflecke@users.noreply.github.com>
2025-08-31 15:30:19 -0400

    Fixing find_module in python

---
+++ snscrape/__init__.py
@@ -0,0 +1,32 @@
+# import pkgutil
+
+
+# __all__ = []
+
+
+# def _import_modules():
+# 	prefixLen = len(__name__) + 1
+# 	for importer, moduleName, isPkg in pkgutil.iter_modules(__path__, prefix = f'{__name__}.'):
+# 		assert not isPkg
+# 		moduleNameWithoutPrefix = moduleName[prefixLen:]
+# 		__all__.append(moduleNameWithoutPrefix)
+# 		module = importer.find_module(moduleName).load_module(moduleName)
+# 		globals()[moduleNameWithoutPrefix] = module
+
+
+# _import_modules()
+
+import pkgutil
+import importlib
+
+all = [] # Define all here
+
+def _import_modules():
+prefixLen = len(name) + 1
+for _, moduleName, _ in pkgutil.iter_modules(path, prefix=f'{name}.'):
+moduleNameWithoutPrefix = moduleName[prefixLen:]
+all.append(moduleNameWithoutPrefix)
+module = importlib.import_module(moduleName)
+
+	globals()[moduleNameWithoutPrefix] = module
+_import_modules()
